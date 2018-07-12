6462e36e66cb67d9147b0f2c67a21151515a1853
tiger19816 <tiger19816@gmail.com>
2018-07-12 09:46:04 +0900

    MainActivityの変更
    各検証画面へ遷移するボタンを実装

--- U22Verification/app/src/main/java/v/team/works/u22/hal/u22verification/MainActivity.java
+++ U22Verification/app/src/main/java/v/team/works/u22/hal/u22verification/MainActivity.java
@@ -1,7 +1,10 @@
 package v.team.works.u22.hal.u22verification;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * ボタンが押された時のイベント処理用メソッド.
+     * Intentをnewする時のの第二引数を各検証画面に変更する。
+     *
+     * @param view
+     */
+    public void onButtonClickListener(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.btVerification01:
+                intent = new Intent(MainActivity.this, MainActivity.class);
+                break;
+            case R.id.btVerification02:
+                intent = new Intent(MainActivity.this, MainActivity.class);
+                break;
+            case R.id.btVerification03:
+                intent = new Intent(MainActivity.this, MainActivity.class);
+                break;
+            case R.id.btVerification04:
+                intent = new Intent(MainActivity.this, MainActivity.class);
+                break;
+            case R.id.btVerification05:
+                intent = new Intent(MainActivity.this, MainActivity.class);
+                break;
+            case R.id.btVerification06:
+                intent = new Intent(MainActivity.this, MainActivity.class);
+                break;
+        }
+        startActivity(intent);
     }
 }
