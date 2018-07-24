c588ad6a6521d0bc6ccb8feeb4975ef72270594b
Yuki Yoshida <osaka.hal.yoshida@gmail.com>
2018-07-24 17:54:50 +0900

    トークンをサーバーに送信する機能を実装中。

--- U22Verification/app/src/main/java/v/team/works/u22/hal/u22verification/Verification01Activity.java
+++ U22Verification/app/src/main/java/v/team/works/u22/hal/u22verification/Verification01Activity.java
@@ -1,8 +1,10 @@
 package v.team.works.u22.hal.u22verification;
 
+import android.annotation.TargetApi;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.annotation.StringRes;
@@ -13,11 +15,15 @@ import android.os.Bundle;
  * 追加インポート。
  */
 import android.util.*;
+import android.view.View;
 
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * サーバからプッシュ通知を受信するクラス。
@@ -32,6 +38,8 @@ import com.google.firebase.messaging.FirebaseMessaging;
  */
 public class Verification01Activity extends AppCompatActivity {
 
+    private String _token;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +50,7 @@ public class Verification01Activity extends AppCompatActivity {
         MyInstanceIDListenerService service = new MyInstanceIDListenerService();
         service.onTokenRefresh();
         FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+        this._token = FirebaseInstanceId.getInstance().getToken();
 
         String channelId = "verification_01";
             String channelName = "notify_verification";
@@ -62,6 +71,44 @@ public class Verification01Activity extends AppCompatActivity {
                 channelName,
                 channelDescription);
         }
+    }
+
+    /**
+     * 編集中。サーバへトークンを送信するメソッド。
+     *
+     * @param view 画面部品。
+     */
+    public void onSendClick(View view) {
+
+//        HttpURLConnection cnct = null;
+//        URL url;
+//        String urlSt = "http://10.0.2.2:8080/U22Verification/Verification01Servlet?token=" + this._token;
+//
+//        try {
+//            // URLの作成
+//            url = new URL(urlSt);
+//            // 接続用HttpURLConnectionオブジェクト作成
+//            cnct = (HttpURLConnection)url.openConnection();
+//            // リクエストメソッドの設定
+//            cnct.setRequestMethod("GET");
+//            // リダイレクトを自動で許可しない設定
+//            cnct.setInstanceFollowRedirects(false);
+//            // URL接続からデータを読み取る場合はtrue
+//            cnct.setDoInput(false);
+//            // URL接続にデータを書き込む場合はtrue
+//            cnct.setDoOutput(false);
+//
+//            // 接続
+//            cnct.connect(); // ①
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            cnct.disconnect();
+//        }
+
+//        HttpResponsAsync cnct = new HttpResponsAsync();
+//        cnct.doInBackground(this._token);
+
     }
 
 
@@ -102,6 +149,7 @@ public class Verification01Activity extends AppCompatActivity {
          * @param channelTitle チャンネル名。
          * @param channelDescription チャンネルの説明。
          */
+        @TargetApi(Build.VERSION_CODES.O)
         public void create(Context context, String channelId, String channelTitle, String channelDescription) {
             String title = channelTitle;
             String description = channelDescription;
@@ -112,6 +160,38 @@ public class Verification01Activity extends AppCompatActivity {
             NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
             notificationManager.createNotificationChannel(channel);
         }
-
     }
+
+//    public class HttpResponsAsync extends AsyncTask<Void, Void, String> {
+//
+//        @Override
+//        protected String doInBackground(String... token) {
+//            HttpURLConnection con = null;
+//            URL url = null;
+//            String urlSt = "http://10.0.2.2:8080/U22Verification/Verification01Servlet?token=" + token;
+//
+//            try {
+//                // URLの作成
+//                url = new URL(urlSt);
+//                // 接続用HttpURLConnectionオブジェクト作成
+//                con = (HttpURLConnection)url.openConnection();
+//                // リクエストメソッドの設定
+//                con.setRequestMethod("POST");
+//                // リダイレクトを自動で許可しない設定
+//                con.setInstanceFollowRedirects(false);
+//                // URL接続からデータを読み取る場合はtrue
+//                con.setDoInput(false);
+//                // URL接続にデータを書き込む場合はtrue
+//                con.setDoOutput(false);
+//
+//                con.connect(); // 接続
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//
+//            return null;
+//        }
+//
+//    }
 }
