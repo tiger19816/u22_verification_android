c8fcb9d679f6b82bf485f52a88b76df1acac5c53
Yuki Yoshida <osaka.hal.yoshida@gmail.com>
2018-07-26 04:46:48 +0900

    サーブレットへトークンを送信する機能を実装。

--- U22Verification/app/src/main/java/v/team/works/u22/hal/u22verification/Verification01Activity.java
+++ U22Verification/app/src/main/java/v/team/works/u22/hal/u22verification/Verification01Activity.java
@@ -1,6 +1,7 @@
 package v.team.works.u22.hal.u22verification;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
@@ -22,6 +23,14 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -37,7 +46,9 @@ import java.net.URL;
  * @author Yuki Yoshida
  */
 public class Verification01Activity extends AppCompatActivity {
-
+    /**
+     * トークンを代入するフィールド。
+     */
     private String _token;
 
     @Override
@@ -45,7 +56,7 @@ public class Verification01Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_01_verification);
 
-        Log.d("アクティビティ","アクティビティ01起動");
+        Log.i("アクティビティ","アクティビティ01起動");
         // 起動アクティビティで実行しなければならないメソッド by Yuki Yoshida.
         MyInstanceIDListenerService service = new MyInstanceIDListenerService();
         service.onTokenRefresh();
@@ -53,8 +64,8 @@ public class Verification01Activity extends AppCompatActivity {
         this._token = FirebaseInstanceId.getInstance().getToken();
 
         String channelId = "verification_01";
-            String channelName = "notify_verification";
-            String channelDescription = "検証01のチャンネル";
+        String channelName = "notify_verification";
+        String channelDescription = "検証01のチャンネル";
 
         // 通知チャンネルの作成 by Yuki Yoshida.
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -79,36 +90,9 @@ public class Verification01Activity extends AppCompatActivity {
      * @param view 画面部品。
      */
     public void onSendClick(View view) {
-
-//        HttpURLConnection cnct = null;
-//        URL url;
-//        String urlSt = "http://10.0.2.2:8080/U22Verification/Verification01Servlet?token=" + this._token;
-//
-//        try {
-//            // URLの作成
-//            url = new URL(urlSt);
-//            // 接続用HttpURLConnectionオブジェクト作成
-//            cnct = (HttpURLConnection)url.openConnection();
-//            // リクエストメソッドの設定
-//            cnct.setRequestMethod("GET");
-//            // リダイレクトを自動で許可しない設定
-//            cnct.setInstanceFollowRedirects(false);
-//            // URL接続からデータを読み取る場合はtrue
-//            cnct.setDoInput(false);
-//            // URL接続にデータを書き込む場合はtrue
-//            cnct.setDoOutput(false);
-//
-//            // 接続
-//            cnct.connect(); // ①
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            cnct.disconnect();
-//        }
-
-//        HttpResponsAsync cnct = new HttpResponsAsync();
-//        cnct.doInBackground(this._token);
-
+        HttpResponsAsync hra = new HttpResponsAsync(this);
+        hra.execute(this._token);
+//        hra.doInBackground(this._token);
     }
 
 
@@ -162,36 +146,143 @@ public class Verification01Activity extends AppCompatActivity {
         }
     }
 
-//    public class HttpResponsAsync extends AsyncTask<Void, Void, String> {
-//
-//        @Override
-//        protected String doInBackground(String... token) {
-//            HttpURLConnection con = null;
-//            URL url = null;
-//            String urlSt = "http://10.0.2.2:8080/U22Verification/Verification01Servlet?token=" + token;
-//
-//            try {
-//                // URLの作成
-//                url = new URL(urlSt);
-//                // 接続用HttpURLConnectionオブジェクト作成
-//                con = (HttpURLConnection)url.openConnection();
-//                // リクエストメソッドの設定
-//                con.setRequestMethod("POST");
-//                // リダイレクトを自動で許可しない設定
-//                con.setInstanceFollowRedirects(false);
-//                // URL接続からデータを読み取る場合はtrue
-//                con.setDoInput(false);
-//                // URL接続にデータを書き込む場合はtrue
-//                con.setDoOutput(false);
-//
-//                con.connect(); // 接続
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//
-//            return null;
-//        }
-//
-//    }
+    public class HttpResponsAsync extends AsyncTask<String, Void, Void> {
+
+        private Activity _activity;
+
+        /**
+         * コンストラクタ。
+         *
+         * @param activity 遷移元アクティビティ。
+         */
+        public HttpResponsAsync(Activity activity) {
+
+            this._activity = activity;
+
+        }
+
+        /**
+         * サーブレットへパラメータを送信するメソッド。
+         *
+         * @param token ①発行されたトークン
+         * @return null
+         */
+        @Override
+        protected Void doInBackground(String... token) {
+            HttpURLConnection cnct = null;
+            URL url = null;
+            OutputStream opStream = null;
+            String urlStr = "http://10.0.2.2:8080/U22Verification/Verification01Servlet";   // 接続先URL
+//            String urlStr = "http://0.0.0.0/hello/hello"; // コネクションエラーがちゃんと出る。
+
+            String postData = "token=" + token[0];  // POSTで送りたいデータ
+
+            Log.d("URL", "アクセス先: " + urlStr + "?" + postData);
+
+            try {
+                url = new URL(urlStr);  // URLの作成
+                cnct = (HttpURLConnection) url.openConnection(); // 接続用HttpURLConnectionオブジェクト作成
+                cnct.setRequestMethod("POST");  // リクエストメソッドの設定
+                cnct.setInstanceFollowRedirects(false); // リダイレクトの可否
+                cnct.setDoOutput(true); // データ書き込みの可否
+                cnct.setReadTimeout(10000); // タイムアウトまでの時間
+                cnct.setConnectTimeout(20000);
+//                cnct.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                cnct.setUseCaches(false);
+
+                cnct.connect(); // 接続
+
+//                JSONObject objToken = new JSONObject();
+//                objToken.put("token", token[0]);
+
+
+                try {
+                    opStream = cnct.getOutputStream();
+
+                    // 送信する値をByteデータに変換する（UTF-8）
+                    opStream.write(postData.getBytes("UTF-8"));
+//                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(opStream, "UTF-8"));
+//                    writer.write(String.valueOf(objToken));
+//                    Log.d("URL", "送信パラメーター: " + writer.toString());
+//                    writer.flush();
+//                    writer.close();
+                    opStream.flush();
+
+                    switch (cnct.getResponseCode()) {
+                        case HttpURLConnection.HTTP_OK :
+                            Log.d("URL", "コネクション状況: 成功");
+                            break;
+                        case HttpURLConnection.HTTP_INTERNAL_ERROR:
+                            Log.e("URL", "エラー内容: 500 内部サーバーエラー");
+                            break;
+                        default:
+                            Log.e("URL", "コネクションレスポンスコード: " + cnct.getResponseCode());
+                            break;
+                    }
+                }
+                catch (Exception e) {
+                    Log.e("URL", "POST送信エラー: " + e.toString());
+                }
+                finally {
+
+                    if(opStream != null) {
+                        try {
+                            opStream.close();
+                        }
+                        catch (Exception e) {
+                            Log.e("URL", "OutputStream解放失敗: " + e.toString());
+                        }
+                    }
+                }
+
+            } catch (Exception e) {
+                Log.e("URL", "コネクションエラー: " + e.toString());
+            } finally {
+                if (cnct != null) {
+                    cnct.disconnect();
+                }
+            }
+
+            return null;
+        }
+
+        /**
+         * サーブレットへアクセスした後に実行されるメソッド。
+         */
+        @Override
+        public void onPostExecute(Void param) {
+            Log.d("HTTP", "onPostExecute: 通過");
+        }
+
+    }
 }
+
+// Logs
+//
+//        07-26 04:33:47.016 13174-13209/v.team.works.u22.hal.u22verification W/OkHttpClient: A connection to http://10.0.2.2:8080/ was leaked. Did you forget to close a response body?
+//        07-26 04:33:47.032 13174-13289/v.team.works.u22.hal.u22verification W/FirebaseMessaging: Error while parsing timestamp in GCM event
+//        java.lang.NumberFormatException: s == null
+//        at java.lang.Integer.parseInt(Integer.java:570)
+//        at java.lang.Integer.parseInt(Integer.java:643)
+//        at com.google.firebase.messaging.zzb.zzc(Unknown Source:81)
+//        at com.google.firebase.messaging.zzb.zze(Unknown Source:2)
+//        at com.google.firebase.messaging.FirebaseMessagingService.zzd(Unknown Source:280)
+//        at com.google.firebase.iid.zzg.run(Unknown Source:26)
+//        at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1162)
+//        at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:636)
+//        at com.google.android.gms.common.util.concurrent.zza.run(Unknown Source:7)
+//        at java.lang.Thread.run(Thread.java:764)
+//        07-26 04:33:47.035 13174-13283/v.team.works.u22.hal.u22verification D/FA: Event not sent since app measurement is disabled
+//        07-26 04:33:47.037 13174-13289/v.team.works.u22.hal.u22verification W/FirebaseMessaging: Error while parsing timestamp in GCM event
+//        java.lang.NumberFormatException: s == null
+//        at java.lang.Integer.parseInt(Integer.java:570)
+//        at java.lang.Integer.parseInt(Integer.java:643)
+//        at com.google.firebase.messaging.zzb.zzc(Unknown Source:81)
+//        at com.google.firebase.messaging.zzb.zzh(Unknown Source:2)
+//        at com.google.firebase.messaging.FirebaseMessagingService.zzd(Unknown Source:321)
+//        at com.google.firebase.iid.zzg.run(Unknown Source:26)
+//        at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1162)
+//        at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:636)
+//        at com.google.android.gms.common.util.concurrent.zza.run(Unknown Source:7)
+//        at java.lang.Thread.run(Thread.java:764)
+//        07-26 04:33:47.035 13174-13283/v.team.works.u22.hal.u22verification D/FA: Event not sent since app measurement is disabled
